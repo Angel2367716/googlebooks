3db31157f868c64f3a2bad830aebe69d87ed7b65
Marco Angel <angel2367716@gmail.com>
2019-10-24 19:03:27 -0700

    added a footer file with style

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Footer.css";
+
+function Footer() {
+  return (
+        <div className="footer">
+			<h6>Author: Marco Angel</h6>
+			<h6>Contact information: <a href="mailto:someone@example.com">
+			angel2367716@outlook.com</a>
+            </h6>
+		</div>
+  );
+}
+
+export default Footer;
