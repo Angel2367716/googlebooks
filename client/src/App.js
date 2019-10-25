8beae032cf1d672bf5ac65dc19af2e33120a6ef9
Marco Angel <angel2367716@gmail.com>
2019-10-24 19:30:54 -0700

    Started to work on routes through react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from './components/Nav';
+import Saved from './pages/Saved';
+import Search from './pages/Search';
 import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div style={{height:'100%'}}>
       <Nav />
+      <Router>
+      <Switch>
+      <Route exact path="/saved" component={Saved} />
+      <Route exact path="/search" component={Search} />
+      </Switch>
+      </Router>
       <Footer />
     </div>
   );
---
+++ client/src/pages/Search/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import "./Search.css";
+
+function Search() {
+  return (
+        <div>
+		Saved page
+		</div>
+  );
+}
+
+export default Search;
